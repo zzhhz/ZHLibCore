8e7efed210ce1d3d25632b5882279f2d04f036ed
墨宇 <zzh_hz@126.com>
2020-03-25 16:55:32 +0800

    优化代码

--- ZZHLibCore/src/main/java/com/zzh/lib/core/utils/HDimenUtils.java
+++ ZZHLibCore/src/main/java/com/zzh/lib/core/utils/HDimenUtils.java
@@ -1,14 +1,5 @@
 package com.zzh.lib.core.utils;
 
-import android.content.Context;
-import android.os.Build;
-import android.util.DisplayMetrics;
-import android.view.WindowManager;
-
-import com.zzh.lib.core.HLibrary;
-
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -34,82 +25,4 @@ public class HDimenUtils {
         Matcher m = p.matcher(mobiles);
         return m.matches();
     }
-
-    /**
-     * 获取屏幕的宽度
-     *
-     * @return 屏幕宽度
-     */
-    public static int getDisplayWidth() {
-        DisplayMetrics dm = HLibrary.getInstance().getContext().getApplicationContext().getResources().getDisplayMetrics();
-        return dm.widthPixels;
-    }
-
-    /**
-     * 获取手机屏幕真实的高度。一般获取手机屏幕的高度是真实手机长度减去状态栏的高度。
-     * getRealMetrics 这个方法是API 17之后才有的，之前的使用@hide 来标记的，并不对外开放API，
-     * 所以API 小于 17使用反射的方法取得屏幕的真实高度
-     *
-     * @return 屏幕高度
-     */
-    public static int getDisplayHeight() {
-        WindowManager wm = (WindowManager) HLibrary.getInstance().getContext().getSystemService(Context.WINDOW_SERVICE);
-        DisplayMetrics dm = new DisplayMetrics();
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
-            wm.getDefaultDisplay().getRealMetrics(dm);
-        } else {
-            try {
-                Class<?> clz = Class.forName("android.view.Display");
-                Method method = clz.getMethod("getRealMetrics", DisplayMetrics.class);
-                method.invoke(wm.getDefaultDisplay(), dm);
-            } catch (ClassNotFoundException e) {
-                e.printStackTrace();
-            } catch (NoSuchMethodException e) {
-                e.printStackTrace();
-            } catch (IllegalAccessException e) {
-                e.printStackTrace();
-            } catch (InvocationTargetException e) {
-                e.printStackTrace();
-            }
-        }
-        return dm.heightPixels;//获取宽度
-    }
-
-    /**
-     * @param px  像素
-     * @return 返回值单位是dp
-     */
-    public static int px2dp(float px) {
-        float scale = HLibrary.getInstance().getContext().getResources().getDisplayMetrics().density;
-        return (int) (px / scale + 0.5f);
-    }
-
-    /**
-     * @param dp  dp值
-     * @return 返回像素值
-     */
-    public static int dp2px(float dp) {
-        float scale = HLibrary.getInstance().getContext().getResources().getDisplayMetrics().density;
-        return (int) (dp * scale + 0.5f);
-    }
-
-    /**
-     * @param px  像素
-     * @return 返回值单位是dp
-     */
-    public static int px2sp(float px) {
-        final float fontScale = HLibrary.getInstance().getContext().getResources().getDisplayMetrics().scaledDensity;
-        return (int) (px / fontScale + 0.5f);
-    }
-
-    /**
-     * @param sp  字体大小
-     * @return 返回字体像素值
-     */
-    public static int sp2px(float sp) {
-        final float fontScale = HLibrary.getInstance().getContext().getResources().getDisplayMetrics().scaledDensity;
-        return (int) (fontScale * sp + 0.5);
-    }
-
-
 }
--- ZZHLibCore/src/main/java/com/zzh/lib/core/utils/HResUtils.java
+++ ZZHLibCore/src/main/java/com/zzh/lib/core/utils/HResUtils.java
@@ -1,8 +1,22 @@
 package com.zzh.lib.core.utils;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.content.res.TypedArray;
+import android.graphics.Point;
+import android.graphics.Rect;
+import android.os.Build;
+import android.util.DisplayMetrics;
+import android.view.Display;
+import android.view.View;
+import android.view.WindowManager;
 
 import com.zzh.lib.core.HLibrary;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 /**
  * Created by Administrator.
@@ -108,5 +122,165 @@ public class HResUtils {
         TypedArray actionbarSizeTypedArray = HLibrary.getInstance().getContext().obtainStyledAttributes(new int[]{android.R.attr.actionBarSize});
         float actionBarHeight = actionbarSizeTypedArray.getDimension(0, 0);
         return (int) actionBarHeight;
+    }
+
+    /**
+     * 获取底部导航栏高度
+     */
+    public static int getNavBarHeight(Activity activity) {
+        int navigationBarHeight = 0;
+        Resources resources = HLibrary.getInstance().getContext().getResources();
+        int resourceId = resources.getIdentifier(isPortrait(activity) ? "navigation_bar_height" : "navigation_bar_height_landscape", "dimen", "android");
+        if (resourceId > 0 && checkDeviceHasNavigationBar(activity) && isNavigationBarVisible(activity)) {
+            navigationBarHeight = resources.getDimensionPixelSize(resourceId);
+        }
+        return navigationBarHeight;
+    }
+    /**
+     * 手机具有底部导航栏时，底部导航栏是否可见
+     */
+    public static boolean isNavigationBarVisible(Activity activity) {
+
+        boolean show = false;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            Display display = activity.getWindow().getWindowManager().getDefaultDisplay();
+            Point point = new Point();
+            display.getRealSize(point);
+            View decorView = activity.getWindow().getDecorView();
+            Configuration conf = activity.getResources().getConfiguration();
+            if (Configuration.ORIENTATION_LANDSCAPE == conf.orientation) {
+                View contentView = decorView.findViewById(android.R.id.content);
+                if (contentView != null) {
+                    show = (point.x != contentView.getWidth());
+                }
+            } else {
+                Rect rect = new Rect();
+                decorView.getWindowVisibleDisplayFrame(rect);
+                show = (rect.bottom != point.y);
+            }
+        }
+        return show;
+    }
+
+    /**
+     * 检测是否具有底部导航栏
+     */
+    public static boolean checkDeviceHasNavigationBar(Activity activity) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            WindowManager windowManager = activity.getWindowManager();
+            Display display = windowManager.getDefaultDisplay();
+            DisplayMetrics realDisplayMetrics = new DisplayMetrics();
+            display.getRealMetrics(realDisplayMetrics);
+            int realHeight = realDisplayMetrics.heightPixels;
+            int realWidth = realDisplayMetrics.widthPixels;
+            DisplayMetrics displayMetrics = new DisplayMetrics();
+            display.getMetrics(displayMetrics);
+            int displayHeight = displayMetrics.heightPixels;
+            int displayWidth = displayMetrics.widthPixels;
+            return (realWidth - displayWidth) > 0 || (realHeight - displayHeight) > 0;
+        } else {
+            boolean hasNavigationBar = false;
+            Resources resources = Resources.getSystem();
+            int id = resources.getIdentifier("config_showNavigationBar", "bool", "android");
+            if (id > 0) {
+                hasNavigationBar = resources.getBoolean(id);
+            }
+            try {
+                Class systemPropertiesClass = Class.forName("android.os.SystemProperties");
+                Method m = systemPropertiesClass.getMethod("get", String.class);
+                String navBarOverride = (String) m.invoke(systemPropertiesClass, "qemu.hw.mainkeys");
+                if ("1".equals(navBarOverride)) {
+                    hasNavigationBar = false;
+                } else if ("0".equals(navBarOverride)) {
+                    hasNavigationBar = true;
+                }
+            } catch (Exception e) {
+            }
+            return hasNavigationBar;
+        }
+    }
+
+    /**
+     * 是否为竖屏
+     */
+    public static boolean isPortrait(Activity activity) {
+        return activity.getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT;
+    }
+
+
+    /**
+     * 获取屏幕的宽度
+     *
+     * @return 屏幕宽度
+     */
+    public static int getDisplayWidth() {
+        DisplayMetrics dm = HLibrary.getInstance().getContext().getApplicationContext().getResources().getDisplayMetrics();
+        return dm.widthPixels;
+    }
+
+    /**
+     * 获取手机屏幕真实的高度。一般获取手机屏幕的高度是真实手机长度减去状态栏的高度。
+     * getRealMetrics 这个方法是API 17之后才有的，之前的使用@hide 来标记的，并不对外开放API，
+     * 所以API 小于 17使用反射的方法取得屏幕的真实高度
+     *
+     * @return 屏幕高度
+     */
+    public static int getDisplayHeight() {
+        WindowManager wm = (WindowManager) HLibrary.getInstance().getContext().getSystemService(Context.WINDOW_SERVICE);
+        DisplayMetrics dm = new DisplayMetrics();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            wm.getDefaultDisplay().getRealMetrics(dm);
+        } else {
+            try {
+                Class<?> clz = Class.forName("android.view.Display");
+                Method method = clz.getMethod("getRealMetrics", DisplayMetrics.class);
+                method.invoke(wm.getDefaultDisplay(), dm);
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            } catch (NoSuchMethodException e) {
+                e.printStackTrace();
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            } catch (InvocationTargetException e) {
+                e.printStackTrace();
+            }
+        }
+        return dm.heightPixels;//获取宽度
+    }
+
+    /**
+     * @param px  像素
+     * @return 返回值单位是dp
+     */
+    public static int px2dp(float px) {
+        float scale = HLibrary.getInstance().getContext().getResources().getDisplayMetrics().density;
+        return (int) (px / scale + 0.5f);
+    }
+
+    /**
+     * @param dp  dp值
+     * @return 返回像素值
+     */
+    public static int dp2px(float dp) {
+        float scale = HLibrary.getInstance().getContext().getResources().getDisplayMetrics().density;
+        return (int) (dp * scale + 0.5f);
+    }
+
+    /**
+     * @param px  像素
+     * @return 返回值单位是dp
+     */
+    public static int px2sp(float px) {
+        final float fontScale = HLibrary.getInstance().getContext().getResources().getDisplayMetrics().scaledDensity;
+        return (int) (px / fontScale + 0.5f);
+    }
+
+    /**
+     * @param sp  字体大小
+     * @return 返回字体像素值
+     */
+    public static int sp2px(float sp) {
+        final float fontScale = HLibrary.getInstance().getContext().getResources().getDisplayMetrics().scaledDensity;
+        return (int) (fontScale * sp + 0.5);
     }
 }
